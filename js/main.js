aef6f8f705d5fc599bb3c4cebdbbb24f392bc2a6
Julia Golovko <ulechka19981998@gmail.com>
2022-10-03 20:51:46 +0300

    написала функцию генерации массива из описаний фото

--- js/main.js
+++ js/main.js
@@ -6,11 +6,46 @@ function getRandom(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-getRandom(1, 10);
-
 function validateStringLength(string, maxLength) {
   return string.length < maxLength;
 }
 
 validateStringLength('hello, World!', 55);
 
+const ADJECTIVE = [
+  'beautiful',
+  'wonderful',
+  'sweet',
+  'impossible',
+  'creative',
+  'sexy',
+];
+
+const NOUN = [
+  'cat',
+  'place',
+  'trip',
+  'human',
+  'food',
+  'girl',
+];
+
+function getRandomWord(array) {
+  return array[getRandom(0, array.length - 1)];
+}
+
+function createPhoto(id) {
+  return {
+    id,
+    url: `photos/${id}.jpg`,
+    description: `${getRandomWord(ADJECTIVE)} ${getRandomWord(NOUN)}`,
+    likes: getRandom(15, 200),
+    comments: getRandom(0, 200),
+  };
+}
+
+function generatePhotosArray(count) {
+  return Array.from({length: count}, (el, index) => createPhoto(index + 1));
+}
+
+generatePhotosArray(25);
